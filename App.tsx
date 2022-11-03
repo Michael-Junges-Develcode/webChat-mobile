299471e70cf1e1a433b9b7271b8a033a9a26e8d2
Michael Junges <michael.junges@develcode.com.br>
2022-11-03 16:53:19 -0300

    switch mui to kitten

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useMemo} from 'react';
 import {useColorScheme} from 'react-native';
 import {DarkTheme, LightTheme} from './src/global/themes/theme';
 import {TopTabs} from './src/routes/TopTabs';
 
 const App = () => {
   const scheme = useColorScheme();
+  const themePreference = scheme === 'dark' ? DarkTheme : LightTheme;
 
   return (
-    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : LightTheme}>
+    <NavigationContainer theme={themePreference}>
       <TopTabs />
     </NavigationContainer>
   );
